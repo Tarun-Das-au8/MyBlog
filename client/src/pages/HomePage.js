86ec7d6442c1f81573f983cb6a1dd2693b0a8377
Tarun-Das-au8 <imtarun79@gmail.com>
2021-03-29 11:01:47 +0530

    Home page

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1>This is HomePage</h1>
+      <p>Hello this is my blog.</p>
+    </div>
+  )
+}
+
+export default HomePage
