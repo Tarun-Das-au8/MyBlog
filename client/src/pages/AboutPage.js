06319f6e8acf208517ef0ce875760a1283366e5d
Tarun-Das-au8 <imtarun79@gmail.com>
2021-03-29 11:02:28 +0530

    about page

---
+++ client/src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>This is about page</h1>
+      <p>Here is my name Tarun Kumar Das</p>
+    </div>
+  )
+}
+
+export default AboutPage
